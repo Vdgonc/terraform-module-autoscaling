be9e648539ca03f3009759514817ca14a4afc16a
Vinicius Justino <justdgoncalves@gmail.com>
2021-05-20 21:38:47 -0300

    buscando todas as subnets da vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ provisioner "aws" {
 data "aws_vpc" "vpc" {
     id = var.vpc_id
 }
+
+data "aws_subnet_ids" "subnets_id" {
+    vpc_id = data.aws_vpc.vpc.id
+}
+
+data "aws_subnet" "subnets" {
+    for_each = data.aws_subnet_ids.subnets_id.ids
+    id = each.value
+}
