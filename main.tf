9da6ff3574a804cd2251c4388942c1c49fcd4077
Vinicius Justino <justdgoncalves@gmail.com>
2021-05-20 21:44:54 -0300

    buscando ubuntu ami id

--- main.tf
+++ main.tf
@@ -14,3 +14,12 @@ data "aws_subnet" "subnets" {
     for_each = data.aws_subnet_ids.subnets_id.ids
     id = each.value
 }
+
+data "aws_ami_ids" "ubuntu" {
+    owners = ["099720109477"]
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/ubuntu-focal-20.04-amd64-server-*"]
+    }
+}
